543fb11d53c2c1ca28cdbe82f6ee4ff281d9a749
Valere <comptedecode@gmail.com>
2026-03-28 16:09:25 -0400

    Working demo with one sensor

--- vesper.ino
+++ vesper.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#define TRIG_PIN 9
+#define ECHO_PIN 10
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Send 10µs pulse to trigger
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
 
+  // Read echo duration in microseconds
+  long duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Convert to cm: sound travels ~343 m/s, divide by 2 for round trip
+  float distance_cm = duration * 0.0343 / 2.0;
+
+  Serial.print("Distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+
+  delay(200);
 }
